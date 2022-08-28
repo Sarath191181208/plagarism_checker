5f398a6fe5fdfb84a3a82be2df83299bbb2e23fa
sarath <vssarathc04@gmail.com>
2022-08-28 19:44:09 +0530

    added null response for exceptions

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+import traceback
 import eel
 from src.PlagarismCecker import check_plagiarism_in_folder
 
@@ -5,10 +7,13 @@ from src.PlagarismCecker import check_plagiarism_in_folder
 eel.init('templates')
 
 @eel.expose
-def demo(file_name: str, extension: str ):
+def uploadFolder(file_name: str, extension: str ):
+    print(file_name, extension)
     try: 
-        return check_plagiarism_in_folder(file_name, extension)
-    except ValueError: # i.e empty folder (or) no files with the given extension
+        return list(check_plagiarism_in_folder(file_name, extension))
+    except ValueError as e: # i.e empty folder (or) no files with the given extension
+        traceback.print_exc()
         return None
+
 # 1000 is width of window and 600 is the height
 eel.start('index.html', size=(1000, 600))
