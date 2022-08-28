4011d5abf0503581f8aa23ee2f444cf09ef3a02b
sarath <vssarathc04@gmail.com>
2022-08-28 20:35:56 +0530

    sorting the response

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from src.PlagarismCecker import check_plagiarism_in_folder
 # name of folder where the html, css, js, image files are located
 eel.init('templates')
 
+def sort_mat(confusion_matrix: list[str, str, float]) -> list[str, str, list]:
+    cm = sorted( confusion_matrix, key= lambda x : x[2], reverse=True)
+    return cm  
+
 @eel.expose
 def uploadFolder(file_name: str, extension: str ):
     print(file_name, extension)
     try: 
-        return list(check_plagiarism_in_folder(file_name, extension))
+        return sort_mat(list(check_plagiarism_in_folder(file_name, extension)))
     except ValueError as e: # i.e empty folder (or) no files with the given extension
         traceback.print_exc()
         return None
