d67a23cdda3cb30a31989ab5c884926aafd089a3
sarath <vssarathc04@gmail.com>
2022-08-29 12:46:55 +0530

    added scroll to top button

---
+++ templates/js/scroll.js
@@ -0,0 +1,18 @@
+let mybutton = document.getElementById("scroll-to-top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.opacity = 1;
+    } else {
+        mybutton.style.opacity = 0;
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function toTopFn() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
