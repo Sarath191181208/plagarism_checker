8d0be509424b71dafb61610d3c1fd6623166584d
sarath <vssarathc04@gmail.com>
2022-10-02 22:16:47 +0530

    -  self from parse as it's static method

--- src/PlagarismCecker/parsers/base_class.py
+++ src/PlagarismCecker/parsers/base_class.py
@@ -2,5 +2,5 @@ from abc import ABC
 
 class BaseParser(ABC):
     @staticmethod
-    def parse(self, abs_file_name: str) -> str:
+    def parse(abs_file_name: str) -> str:
         """reads and returns the text of the given file"""
