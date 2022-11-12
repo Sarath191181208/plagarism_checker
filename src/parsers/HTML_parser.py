c7cbb1dc92d34d9a1044c1956ff40b56aa71e648
sarath <vssarathc04@gmail.com>
2022-11-12 21:05:38 +0530

    refactored html parser

--- src/parsers/HTML_parser.py
+++ src/parsers/HTML_parser.py
@@ -4,13 +4,17 @@ class WordParser(BaseParser):
     @staticmethod
     def parse(abs_file_name: str) -> str:
         with open(abs_file_name, mode='rb') as f:
-            
-            soup = BeautifulSoup(html, features="html.parser")
+            return WordParser.parse_html(BeautifulSoup(f, 'html.parser'))
 
-            for script in soup(["script", "style"]):
-               script.extract()   
-            text = soup.get_text()
-            lines = (line.strip() for line in text.splitlines())
-            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            text = '\n'.join(chunk for chunk in chunks if chunk)
+    @staticmethod
+    def parse_html(html_content: str)  -> str:
+        soup = BeautifulSoup(html_content, features="html.parser")
+
+        # remove all javascript and stylesheet code
+        for script in soup(["script", "style"]): script.extract()  
+
+        text = soup.get_text()
+        lines = (line.strip() for line in text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        text = '\n'.join(chunk for chunk in chunks if chunk)
         return(text)
