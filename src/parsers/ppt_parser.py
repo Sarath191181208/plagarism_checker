2e9c6c9a8d431fb16c8d9b9a03eef7c3109ce708
Nithishkumar193 <nitnithish193@gmail.com>
2022-11-12 22:48:25 +0530

    changed ppt parsers

--- src/parsers/ppt_parser.py
+++ src/parsers/ppt_parser.py
@@ -1,18 +1,24 @@
 from pptx import Presentation 
-import glob
 from .base_class import BaseParser
 
 class PptParser(BaseParser):
     @staticmethod
     def parse(abs_file_name: str) -> str:
-        with open(abs_file_name, mode='rb') as f:
-            for eachfile in glob.glob("*.pptx"):
-               prs = Presentation(eachfile)
-               print(eachfile)
-            for slide in prs.slides:
-                for shape in slide.shapes:
-                    if hasattr(shape, "text"):
-                        print(shape.text)
+            
+               prs = Presentation(abs_file_name)
+               text_runs=[]
+               for slide in prs.slides:
+                   for shape in slide.shape:
+                      if not shape.has_text_frame:
+                         continue
+                      for paragraph in shape.text_frame.paragraphs:
+                          for run in paragraph.runs:
+                               text_runs.append(run.text)
+               return '\n'.join(text_runs)
+
+                
+                
+                
 
 
 
