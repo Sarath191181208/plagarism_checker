88e23bf2bbb5ce5a9937e777f5fef21d8330fcc9
Nithishkumar193 <nitnithish193@gmail.com>
2022-11-12 13:45:03 +0530

    Implemented doc and excel parser

---
+++ src/parsers/excel_parser.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from .base_class import BaseParser
+
+class ExcelParser(BaseParser):
+    @staticmethod
+    def parse(abs_file_name: str) -> str:
+        with open(abs_file_name, mode='rb') as f:
+           excelreader=pd.read_excel(f)
+        return(excelreader)
+
+if __name__ == "__main__":
+    ExcelParser.parse()
+
+
+           
